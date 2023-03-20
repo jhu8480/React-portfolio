f62a0c10b1655dbc3a77b0cea0bab92b5f1c7832
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-20 18:29:22 -0400

    added elements in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='container'>
+      <div className='home-container'>
+        <h1 className='greeting'>Hi! My name is Jinhui</h1>
+        <p>I'm a <b>web developer</b> looking for an entry-level web dev job in Toronto, ON</p>
+        <div className='skills'>
+          <div className='list'>
+            <h2>Front end skills</h2>
+            <p>Html, CSS, JavaScript, JQuery, React JS, Bootstrap, Tailwind CSS</p>
+          </div>
+          <div className='list'>
+            <h2>Back end skills</h2>
+            <p>Node, Express JS, MySQL, MongoDB, Sequelize, Mongoose</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
