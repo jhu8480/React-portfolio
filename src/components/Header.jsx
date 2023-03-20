7f75b4da5e3808636e5e3946e84ef8463ccc6006
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-20 14:47:30 -0400

    created Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Header.css';
 
-function Header() {
+function Header({currentPage, handlePageChange}) {
   return (
-    <div>Header</div>
+    <div className='navbar'>
+      <button onClick={() => {handlePageChange('Home')}}className={currentPage === 'Home' ? 'active' : ''}>Home</button>
+      <button onClick={() => {handlePageChange('Projects')}}className={currentPage === 'Projects' ? 'active' : ''}>Projects</button>
+      <button onClick={() => {handlePageChange('Contact')}}className={currentPage === 'Contact' ? 'active' : ''}>Contact</button>
+      <button onClick={() => {handlePageChange('Resume')}}className={currentPage === 'Resume' ? 'active' : ''}>Resume</button>
+    </div>
   )
 }
 
