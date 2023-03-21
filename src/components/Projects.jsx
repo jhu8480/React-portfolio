1a3e047be772d7515a55a9ec7a6d6eaf64ac4532
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-21 19:45:25 -0400

    added projects in projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,9 +11,18 @@ function Projects() {
           <a href="https://stack-under-flow.herokuapp.com/" target="_blank" rel="noreferrer">Blog Website</a>
           <a href="https://github.com/jhu8480/stack-under-flow" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
         </div>
-        <div className='project'></div>
-        <div className='project'></div>
-        <div className='project'></div>
+        <div className='project project-2'>
+          <a href="https://jhu8480.github.io/timed-quiz-app/" target="_blank" rel="noreferrer">Timed Quiz</a>
+          <a href="https://github.com/jhu8480/timed-quiz-app" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+        </div>
+        <div className='project project-3'>
+        <a href="https://jhu8480.github.io/Coding-Challenge-Recipe-App/" target="_blank" rel="noreferrer">Recipe App</a>
+          <a href="https://github.com/jhu8480/Coding-Challenge-Recipe-App" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+        </div>
+        <div className='project project-4'>
+        <a href="https://jhu8480.github.io/weather-app/" target="_blank" rel="noreferrer">Weather App</a>
+          <a href="https://github.com/jhu8480/Weather-App" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+        </div>
       </div>
     </div>
   )
