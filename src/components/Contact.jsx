bd95791ee6360c46e509ae9dadaf8f073fcdd33d
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-20 20:07:35 -0400

    added event listeners in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import '../styles/Contact.css';
+import { validateEmail } from '../utils/helpers';
 
 function Contact() {
   const [username, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
     const {target} = e;
@@ -13,18 +15,30 @@ function Contact() {
 
     if (inputType === 'email') {
       setEmail(inputValue);
-      console.log(email);
     } else if (inputType === 'username') {
       setUserName(inputValue);
-      console.log(username);
     } else {
       setMessage(inputValue);
-      console.log(message);
+    }
+  }
+
+  const handleBlur = () => {
+    if(!validateEmail(email)) {
+      setErrorMessage('Invalid Email');
+      return;
+    } else if (!username || !message) {
+      setErrorMessage('Please complete the form');
     }
   }
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    
+    if (!validateEmail(email)) {
+      setErrorMessage('Please enter a valid Email Address');
+      return;
+    }
+
     console.log({
       username,
       email,
@@ -38,13 +52,18 @@ function Contact() {
         <h2>Contact</h2>
         <form>
           <p>Name</p>
-          <input type="text" name="username" placeholder='name' onChange={handleInputChange}/>
+          <input type="text" name="username" placeholder='name' onChange={handleInputChange} onFocus={() => setErrorMessage('')} onBlur={handleBlur}/>
           <p>Email</p>
-          <input type="email" name="email" placeholder='email' onChange={handleInputChange}/>
+          <input type="email" name="email" placeholder='email' onChange={handleInputChange} onFocus={() => setErrorMessage('')} onBlur={handleBlur}/>
           <p>Message</p>
-          <textarea cols="30" rows="10" placeholder='enter a message here...' onChange={handleInputChange}></textarea><br />
+          <textarea cols="30" rows="10" placeholder='enter a message here...' onChange={handleInputChange} onFocus={() => setErrorMessage('')} onBlur={handleBlur}></textarea><br />
           <button onClick={handleFormSubmit}>Submit</button>
         </form>
+        {errorMessage ? 
+        <div>
+          <p>{errorMessage}</p>
+        </div> : <div></div>
+        }
       </div>
     </div>
   )
