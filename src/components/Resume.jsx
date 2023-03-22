d365d7a86f6e573104cc9d3f50f74318aa271393
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-21 21:09:28 -0400

    added vertical timeline package

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
 function Resume() {
-
-
-
   return (
-    <div className='container'>
-    </div>
+      <a href={process.env.PUBLIC_URL + '/test.txt'} download>Download File</a>
   )
 }
 
