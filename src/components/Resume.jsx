4b754db61fe6559d62f0f51d244a7fe89834987b
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-21 21:39:44 -0400

    added timeline in resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../styles/Resume.css'
 
 function Resume() {
   return (
-      <a href={process.env.PUBLIC_URL + '/test.txt'} download>Download File</a>
+      <VerticalTimeline lineColor='#2d3439'>
+        <VerticalTimelineElement iconStyle={{background: '#2d3439', color: '#fff'}}>
+        <a className='download-link' href={process.env.PUBLIC_URL + '/test.txt'} download>Download my resume</a>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className='vertical-timeline-element--education' date='2010 - 2014' iconStyle={{background: '#2d3439', color: '#fff'}}>
+          <p>University of Shanghai for Science and Technology</p>
+          <p>Bachelor of Arts / Events Management</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className='vertical-timeline-element--education' date='2014 - 2015' iconStyle={{background: '#2d3439', color: '#fff'}}>
+          <p>University of Maryland College Park</p>
+          <p>Master of Science / Supply Chain Management</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className='vertical-timeline-element--education' date='2015 - 2016' iconStyle={{background: '#2d3439', color: '#fff'}}>
+          <p>WMGS (Shanghai) Co., Ltd.</p>
+          <p>Replenishment Analyst</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className='vertical-timeline-element--education' date='2016 - 2022' iconStyle={{background: '#2d3439', color: '#fff'}}>
+          <p>Hangzhou Hongzhaochang Technology Co., Ltd.</p>
+          <p>Technical Sales Specialist</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
   )
 }
 
