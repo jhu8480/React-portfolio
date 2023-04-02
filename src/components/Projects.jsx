b71f9ee4c208a51a02de09ff2a7eff8531888b9d
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-04-02 18:41:01 -0400

    added resume, adjusted projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ function Projects() {
     <div className='container'>
       <div className='projects-container'>
         <div className='project project-1'>
-          <a href="https://stack-under-flow.herokuapp.com/" target="_blank" rel="noreferrer">Blog Website</a>
+          <a href="https://sic-ecommerce.herokuapp.com/" target="_blank" rel="noreferrer">Shopping Website</a>
           <a href="https://github.com/jhu8480/stack-under-flow" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
         </div>
         <div className='project project-2'>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,7 +7,7 @@ function Resume() {
   return (
       <VerticalTimeline lineColor='#2d3439'>
         <VerticalTimelineElement iconStyle={{background: '#2d3439', color: '#fff'}}>
-        <a className='download-link' href={process.env.PUBLIC_URL + '/test.txt'} download>Download my resume</a>
+        <a className='download-link' href={process.env.PUBLIC_URL + '/Jinhui_Hu_Resume_w.pdf'} download>Download my resume</a>
         </VerticalTimelineElement>
         <VerticalTimelineElement className='vertical-timeline-element--education' date='2010 - 2014' iconStyle={{background: '#2d3439', color: '#fff'}}>
           <p>University of Shanghai for Science and Technology</p>
